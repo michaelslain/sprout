9151c2f255af8895ac033799f64b4df7554c8dad
michaelslain <misha.slain@icloud.com>
2022-02-20 14:26:18 -0800

    Added highlighter.jsx to index.js

--- index.js
+++ index.js
@@ -8,4 +8,5 @@ module.exports = {
     operators: require('./operators.js'),
     parse: require('./parse.js'),
     tokenize: require('./tokenize.js'),
+    highlighter: require('./highlighter.jsx'),
 }
